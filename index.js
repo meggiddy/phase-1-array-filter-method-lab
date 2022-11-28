ee8eb5f636a3c82522e2ba1939c8e3bbae74bdf1
Maggie <muriithinyawira13@gmail.com>
2022-11-28 12:32:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+var findMatching = (array, driver)=>{
+    return array.filter(drivers => drivers.toLowerCase() === driver.toLowerCase());
+}
+var fuzzyMatch = (array, str)=>{
+    return array.filter(drivers => drivers.startsWith(str));
+}
+var matchName = (array, driver)=>{
+    return array.filter(({name, hometown} )=> name === driver);
+}
